dbd24883abd93a32180f9ada3d4326c803e93b95
Ali Shehbaz <alishahbaz072@gmail.com>
2023-12-09 22:23:58 -0500

    add gpt4 vision

--- server/index.js
+++ server/index.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const OpenAI = require("openai");
 
+require("dotenv").config();
 const app = express();
 const port = 3000;
+
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY });
 
 // Middleware to parse JSON data from requests
 app.use(bodyParser.json());
 
 // Placeholder data (you can replace this with a database or any other data source)
 let data = [
-  { id: 1, name: "Item 1" },
+  { id: 1, ame: "Item 1" },
   { id: 2, name: "Item 2" },
   { id: 3, name: "Item 3" },
 ];
 
 // GET request to retrieve all items
 app.get("/api/items", (req, res) => {
+  console.log(process.env.OPEN_AI_KEY);
   res.json(data);
 });
 
@@ -31,7 +36,44 @@ app.get("/api/items/:id", (req, res) => {
   }
 });
 
-// POST request to add a new item
+// GET request for gpt vision api call
+app.get("/api/evaluateImage", async (req, res) => {
+  //   const itemId = parseInt(req.params.id);
+  //   const item = data.find((item) => item.id === itemId);
+
+  console.log("eval image");
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "What’s in this image? give me the edible components in this picture as a list of comma separated values.",
+          },
+          {
+            type: "image_url",
+            image_url: {
+              url: "https://pinchofyum.com/wp-content/uploads/Chicken-Sweet-Potato-Meal-Prep-Bowls-Recipe.jpg",
+            },
+          },
+        ],
+      },
+    ],
+    max_tokens: 300,
+  });
+  console.log(response.choices[0]);
+
+  const listOfFoodItems = response.choices[0].message.content;
+
+  console.log(listOfFoodItems);
+
+  res.json(listOfFoodItems);
+});
+
+// // POST request to add a new item
 app.post("/api/items", (req, res) => {
   const newItem = req.body;
 
