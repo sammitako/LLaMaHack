65e64bf789c21a7282a80c7b7dbd1403f2b8ce63
Ali Shehbaz <alishahbaz072@gmail.com>
2023-12-10 07:30:16 -0500

    add edamam wrapper

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const OpenAI = require("openai");
 const cors = require("cors");
-require("dotenv").config();
+const axios = require("axios");
 
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 app.use(cors());
-
 
 const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY });
 
@@ -40,13 +40,7 @@ app.get("/api/items/:id", (req, res) => {
   }
 });
 
-// GET request for gpt vision api call
-app.get("/api/evaluateImage", async (req, res) => {
-  //   const itemId = parseInt(req.params.id);
-  //   const item = data.find((item) => item.id === itemId);
-
-  console.log("eval image");
-
+const getIngredientsListFromGPT4 = async (url) => {
   const response = await openai.chat.completions.create({
     model: "gpt-4-vision-preview",
     messages: [
@@ -55,12 +49,12 @@ app.get("/api/evaluateImage", async (req, res) => {
         content: [
           {
             type: "text",
-            text: "What’s in this image? give me the edible components in this picture as a list of comma separated values.",
+            text: "Please analyze the attached image of a food dish. Identify all visible food items and their estimated quantities. Provide the results in a list of ingredients with measurements in imperial units, using common kitchen measures like cups, ounces, tablespoons, etc. For example, '1 cup rice, 10 oz chickpeas'. Provide a best-guess estimate if exact measurements are not possible. Assume standard serving sizes for each food item.don't provide any text beyond the ingredients that are present in the image. Also don't provide information such as cubed, sliced, halved etc.",
           },
           {
             type: "image_url",
             image_url: {
-              url: "https://pinchofyum.com/wp-content/uploads/Chicken-Sweet-Potato-Meal-Prep-Bowls-Recipe.jpg",
+              url: url,
             },
           },
         ],
@@ -72,9 +66,59 @@ app.get("/api/evaluateImage", async (req, res) => {
 
   const listOfFoodItems = response.choices[0].message.content;
 
-  console.log(listOfFoodItems);
+  return listOfFoodItems;
+};
 
-  res.json(listOfFoodItems);
+const getNutritionData = async (foodItems) => {
+  console.log("https://api.edamam.com/api/nutrition-data");
+
+  let totalCalories = 0;
+
+  const axiosReq = foodItems.map(async (item) => {
+    try {
+      const response = await axios.get(
+        "https://api.edamam.com/api/nutrition-data?app_id=" +
+          encodeURIComponent(process.env.EDAMAM_APP_ID) +
+          "&app_key=" +
+          encodeURIComponent(process.env.EDAMAM_APP_KEY) +
+          "&nutrition-type=logging&ingr=" +
+          encodeURIComponent(item)
+      );
+      console.log(response.data.ingredients);
+      console.log(response.data.calories);
+      totalCalories += response.data.calories;
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  // Wait for all promises to resolve
+  await Promise.all(axiosReq);
+
+  console.log(foodItems);
+  console.log("total calories are", +totalCalories);
+};
+
+// GET request for gpt vision api call
+app.get("/api/evaluateImage", async (req, res) => {
+  //   const itemId = parseInt(req.params.id);
+  //   const item = data.find((item) => item.id === itemId);
+
+  console.log("eval image");
+
+  foodItems = await getIngredientsListFromGPT4(
+    "https://www.eatthis.com/wp-content/uploads/sites/4/2019/11/thanksgiving-turkey-dinner-plate.jpg?quality=82&strip=1"
+  );
+
+  //   foodItems =
+  //     "lettuce, cherry tomatoes, hard-boiled eggs, chicken breast, avocado, bacon, blue cheese crumbles, chives.";
+
+  foodItems = foodItems.replace(/\n/g, "").split("- ").filter(Boolean);
+
+  //   const foodItems = ["3 oz turkey"];
+  nutritionData = getNutritionData(foodItems);
+
+  res.json(foodItems);
 });
 
 // // POST request to add a new item
